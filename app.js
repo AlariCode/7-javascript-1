18d075aa5ecf04bbaebfbf815fa72607ca0219c7
Alaricode <antonlarichev@gmail.com>
2022-07-27 13:32:30 +0300

    Арифметические операторы

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-let a = 1;
-console.log(a);
-a = 'test';
-console.log(a);
+// Базовые арифметические операторы
+const width = 10;
+const height = 5;
+const space = width * height;
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth / newWidth2;
+const volume = 2 ** 3; // 2 * 2 * 2
+console.log(volume);
+
+// Строки
+const city = 'Москва';
+const street = 'Новослободская';
+console.log(city + ', ' + street + ' ' + 5);
