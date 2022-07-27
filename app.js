4be2d3db7ac3b6536c9e73f152b4df19d3cc5d36
Alaricode <antonlarichev@gmail.com>
2022-07-27 13:41:22 +0300

    Операторы присваивания и сравнения

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-// Базовые арифметические операторы
-const width = 10;
-const height = 5;
-const space = width * height;
-const newWidth = width - 4;
-const newWidth2 = width + 4;
-const division = newWidth / newWidth2;
-const volume = 2 ** 3; // 2 * 2 * 2
-console.log(volume);
+// Операторы присваивания
+let age = 18 + 5;
+age += 2; // age = age + 2
+age -= 3; // age = age - 2
+age *= 2; // age = age * 2
+age /= 2; // age = age / 2
 
-// Строки
-const city = 'Москва';
-const street = 'Новослободская';
-console.log(city + ', ' + street + ' ' + 5);
+age++; // age = age + 1
+age--; // age = age - 1
+console.log(age);
+
+// Операторы сравнения
+const vasia = 20;
+console.log(age > vasia);
+console.log(age >= vasia);
+console.log(age < vasia);
+console.log(age <= vasia);
+console.log(age == vasia);
