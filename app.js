fcd542f9a2a9425b4ad49c4195a60f3a281ff020
Alaricode <antonlarichev@gmail.com>
2022-07-27 18:28:03 +0300

    Типы данных

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const isSuited = 100 - 10 > 90 - 5;
-console.log(isSuited);
+let a = 5;
+let b = 5.6;
+console.log(typeof a);
+console.log(typeof b);
+a = 'строка';
+console.log(typeof a);
+e = 'sdfsf'
+console.log(typeof e);
 
-// 100 - (12) 10 > (10) 90 - (12) 5
+let isAdmin = a > 10;
+console.log(typeof isAdmin);
 
-const a = (6 + 10) / 2;
-console.log(a);
+let c = undefined;
+console.log(typeof c);
+c = 5;
+console.log(typeof c);
 
-let b;
-let c;
-c = b = 100 + 50 + 30;
-console.log(c);
-console.log(b);
+let d = null;
+console.log(typeof d == null);
