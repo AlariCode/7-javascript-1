f5b3845054a277cbab57a3f363d50d04e982e8d6
Alaricode <antonlarichev@gmail.com>
2022-07-28 13:28:10 +0300

    Упражнение - Расчёт проекта

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-let a = 5;
-let b = 5.6;
-console.log(typeof a);
-console.log(typeof b);
-a = 'строка';
-console.log(typeof a);
-e = 'sdfsf'
-console.log(typeof e);
+/*
+Ваша часовая ставка 80$ и вы готовы работать не
+более 5 часов в день 5 дней в неделю (кроме выходных).
+К вам приходит заказчик и предлагает заказ на 40
+часов работы.
+Сейчас понедельник.
+Вы должны уехать через 11 дней.
+Выведете в консоль:
+- Boolean переменную успеете ли вы взяться за работу
+- Сколько вы за неё попросите?
+*/
 
-let isAdmin = a > 10;
-console.log(typeof isAdmin);
+// Данные
+const payRateUSD = 80;
+const projectHours = 40;
+const availableHours = (11 - 2) * 5;
 
-let c = undefined;
-console.log(typeof c);
-c = 5;
-console.log(typeof c);
-
-let d = null;
-console.log(typeof d == null);
+// Результат
+console.log('Смогу ли я работать? ' + (availableHours > projectHours));
+console.log('Стоимость работ: ' + projectHours * payRateUSD + '$');
