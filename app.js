f742aab9784389a8cf7a1ce32e96e9d87d73ca9d
Alaricode <antonlarichev@gmail.com>
2022-08-01 11:43:44 +0300

    If else

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-console.log(Boolean(0));
-console.log(Boolean(''));
-console.log(Boolean(undefined));
-console.log(Boolean(null));
-console.log(Boolean(NaN));
+const money = 100;
+
+if (money > 50) {
+	console.log('Может купить наш продукт');
+} else if (money > 50) {
+	console.log('Куплен mini продукт');
+} else if (money > 50) {
+	console.log('Куплен mini продукт');
+} else {
+	console.log('Не хватает баланса');
+}
+
+console.log('Итог');
