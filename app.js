6229361f324580974877a86bbc22741fafea5729
Alaricode <antonlarichev@gmail.com>
2022-08-01 11:14:47 +0300

    False из других типов

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-const age = '18';
-console.log(Number(age) + 5);
-console.log(age * 3);
-const userName = 'Вася';
-console.log(Number(userName) + 5);
-console.log(typeof NaN);
-console.log(String(4) + 7);
-console.log(Boolean('') + 10);
-console.log(true + 2);
-
-const a = 2 + '10';
-console.log(a - 10);
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
