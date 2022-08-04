e90bc14b94b1e83ca00d151a7247efd6ae44921d
Alaricode <antonlarichev@gmail.com>
2022-08-04 14:57:46 +0300

    Тернарные операторы

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
-const role = 'manager';
+const bmwX3Price = 100000;
+const fordFocusPrice = 10000;
+const budget = 20000;
 
-switch (role) {
-	case 'manager':
-		console.log('Менеджер');
-		break;
-	case 'admin':
-		console.log('Админ');
-		break;
-	case 'ceo':
-		console.log('CEO');
-		break;
-	default:
-		console.log('Мы тебя не знаем!');
-}
+let message = budget > bmwX3Price ? 'BMW' : 'Велосипед';
 
-switch (role) {
-	case 'manager': // role === 'manager'
-	case 'admin':
-		console.log('Не руководитель');
-		break;
-	case 'ceo':
-		console.log('Руководитель');
-		break;
-	default:
-		console.log('Мы тебя не знаем!');
-}
-
-const num = 1;
-
-switch(true) {
-	case num > 0:
-		console.log('Положительный');
-		break;
-	case num < 0:
-		console.log('Отрицательный');
-		break;
-	default:
-		console.log('Ноль!');
-}
+console.log(`Я хочу купить ${message}`);
