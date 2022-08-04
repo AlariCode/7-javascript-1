39e489b8193bae8d3476fd996d227d64715e846b
Alaricode <antonlarichev@gmail.com>
2022-08-04 16:44:13 +0300

    Оператор нулевого слияния

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-console.log('Вася' || 'Олег');
-console.log(false || 'Олег');
-console.log('Вася' || false);
-console.log(false || false);
-
-console.log('Вася' && 'Олег');
-console.log(false && 'Олег');
-console.log('Вася' && false);
-console.log(false && false);
-
-let a = 'Марина';
-const username = a || 'Петя';
-console.log(username);
-
-const isAdmin = true;
-const filename = isAdmin && 'file.mp4';
-console.log(filename);
+let age = 0;
+console.log(age || 18);
+console.log(age ?? 18);
