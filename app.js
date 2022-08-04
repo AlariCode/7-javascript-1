d1e19a50ffa62af73b817c183dbfc9a526916abc
Alaricode <antonlarichev@gmail.com>
2022-08-04 15:42:23 +0300

    Упражнение - Проверка робота

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-const bmwX3Price = 100000;
-const fordFocusPrice = 10000;
-const budget = 20000;
+/*
+	Методом prompt получите ответ пользователя
+	на вопрос "Сколько будет 7 + или - 15?". Если ответ верен
+	выведите в консоле "Успех", если нет - "Вы робот!",
+	а если он введёт "Я не робот", то тоже "Успех".
+*/
+const res = prompt('Сколько будет 7 + или - 15?');
+switch(true) {
+	case res === 'Я не робот':
+	case Number(res) === 22:
+	case Number(res) === -8:
+		console.log('Успех');
+		break;
+	default:
+		console.log('Вы робот!');
+}
 
-let message = budget > bmwX3Price ? 'BMW' : 'Велосипед';
-
-console.log(`Я хочу купить ${message}`);
+if (res === 'Я не робот') {
+	console.log('Успех');
+} else {
+	const resNum = Number(res);
+	switch(resNum) {
+		case 22:
+		case -8:
+			console.log('Успех');
+			break;
+		default:
+			console.log('Вы робот!');
+	}
+}
