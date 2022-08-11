1d1ed5f1876d599823b87d2641424553cfd39cea
Alaricode <antonlarichev@gmail.com>
2022-08-11 17:30:49 +0300

    Функции в функциях

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-function canAccessWebsite(age) {
-	if (age < 18) {
-		return 'Нет';
-	}
-	return 'Да';
+const KG_IN_USD = 7;
+const KM_IN_USD = 5;
+
+function calculateW(present) {
+	return present * KG_IN_USD;
 }
 
-console.log(canAccessWebsite(19));
+function calculateKm(distance) {
+	return distance * KM_IN_USD;
+}
 
-const canAccessWebsite2 = age => age < 18 ? 'Нет' : 'Да';
+function getExchagePrice(present1, present2, distance) {
+	const price1 = calculateW(present1);
+	const price2 = calculateW(present2);
+	const distancePrice = calculateKm(distance);
+	return price1 + price2 + distancePrice;
+}
 
-console.log(canAccessWebsite2(19));
+console.log(getExchagePrice(1,2,10));
