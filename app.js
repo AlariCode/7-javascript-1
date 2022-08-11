f877975a334b9c7710a696a5d5f3b882d2638cf9
Alaricode <antonlarichev@gmail.com>
2022-08-11 16:58:31 +0300

    Параметры по умолчанию

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-/* Переписать функцию в стрелочную */
-
-function toPower(num, power) {
+function toPower(num, power = 2) {
 	const res = num ** power;
 	return res;
 }
 
 console.log(toPower(2, 3));
-
-const toPowerArrow = (num, power) => num ** power;
-console.log(toPowerArrow(2, 3));
+console.log(toPower(2));
