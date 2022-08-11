21c6672bcc2447474220e7591d745f8f2116ddb8
Alaricode <antonlarichev@gmail.com>
2022-08-11 16:41:56 +0300

    Стрелочные функции

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 function powerOfTwo(num) {
+	console.log(num);
 	return num * num;
 }
 console.log(powerOfTwo(5));
 
-const poft = function (num) {
-	return num * num;
-}
+const poft = num => num * num;
 console.log(poft(6));
