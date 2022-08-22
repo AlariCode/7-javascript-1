45bb88e257c3b9a9155eb54b24d9e7ad3729922d
Alaricode <antonlarichev@gmail.com>
2022-08-22 17:28:06 +0300

    Деструктуризация

--- app.js
+++ app.js
@@ -1,39 +1,12 @@
-/*
-	Дан список задач
-	const tasks = ['Задача 1'];
-	Сделать функции:
-	- Добавление задачи в конец
-	- Удаление задачи по названию
-	- Перенос задачи в начало списка по названию 
-	Всегда меняем исходный массив
-*/
+const userData = ['Антон', 18, 'Москва'];
 
-const tasks = ['Задача 1'];
-
-function Add(task) {
-	tasks.push(task);
+function getData() {
+	return ['Антон', 18, 'Москва'];
 }
+// const userName = getData()[0];
+// const age = getData()[1];
+// const city = getData()[2];
 
-function Remove(task) {
-	const index = tasks.indexOf(task);
-	if (index === -1) {
-		return;
-	}
-	return tasks.splice(index, 1);
-}
+const [userName, _, city] = userData;
 
-function Prioritize(task) {
-	const result = Remove(task);
-	if (!result) {
-		return;
-	}
-	tasks.unshift(result[0]);
-}
-
-Add('Задача 2');
-Add('Задача 3');
-console.log(tasks);
-Remove('Задача 2');
-console.log(tasks);
-Prioritize('Задача 3');
-console.log(tasks);
+console.log(userName, city);
