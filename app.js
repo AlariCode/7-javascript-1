6da6e8f75442e54287ae7fdf366bc23d06a0b5c8
Alaricode <antonlarichev@gmail.com>
2022-08-22 17:47:29 +0300

    Rest оператор

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const userData = ['Антон', 18, 'Москва'];
-
-function getData() {
-	return ['Антон', 18, 'Москва'];
-}
-// const userName = getData()[0];
-// const age = getData()[1];
-// const city = getData()[2];
-
-const [userName, _, city] = userData;
-
-console.log(userName, city);
+const data = [1, 2, 3, 4, 5, 6, 7];
+const [one, two, ...others] = data;
+console.log(one, two, others);
