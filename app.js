702bff57252b07a92b5e8e70585fdee7bfeace9c
Alaricode <antonlarichev@gmail.com>
2022-08-22 16:20:25 +0300

    Упражнение - Обновление списка задач

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
-const roles = ['user', 'admin', 'manager', 'superuser'];
+/*
+	Дан список задач
+	const tasks = ['Задача 1'];
+	Сделать функции:
+	- Добавление задачи в конец
+	- Удаление задачи по названию
+	- Перенос задачи в начало списка по названию 
+	Всегда меняем исходный массив
+*/
 
-const url = 'auth/user/login';
-const res = url.split('/');
-console.log(res);
+const tasks = ['Задача 1'];
 
-console.log(roles.join('-'));
+function Add(task) {
+	tasks.push(task);
+}
+
+function Remove(task) {
+	const index = tasks.indexOf(task);
+	if (index === -1) {
+		return;
+	}
+	tasks.splice(index, 1);
+}
+
+function Prioritize(task) {
+	const index = tasks.indexOf(task);
+	if (index === -1) {
+		return;
+	}
+	const oldTask = tasks[index];
+	tasks.splice(index, 1);
+	tasks.unshift(oldTask);
+}
+
+Add('Задача 2');
+Add('Задача 3');
+console.log(tasks);
+Remove('Задача 4');
+console.log(tasks);
+Prioritize('Задача 4');
+console.log(tasks);
