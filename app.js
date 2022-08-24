ef8f0d04703048bde8fb4e35d6ebd44a37335fa4
Alaricode <antonlarichev@gmail.com>
2022-08-24 18:55:23 +0300

    Break и continue

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-// console.log('Наш баланс 1$');
-// console.log('Наш баланс 2$');
-// console.log('Наш баланс 3$');
-// console.log('Наш баланс 4$');
-// console.log('Наш баланс 5$');
-// console.log('Наш баланс 6$');
-// console.log('Наш баланс 7$');
-// console.log('Наш баланс 8$');
-// console.log('Наш баланс 9$');
+const tasks = ['Задача 1', 'Задача 2', 'Задача 3'];
 
-for(let i = 1; i < 10; i++) {
-	console.log(`Наш баланс ${i}$`);
+
+for(let i = 0; i < tasks.length; i++) {
+	if (tasks[i] === 'Задача 2') {
+		continue;
+	}
+	console.log(tasks[i]);
+}
+console.log('-----')
+for(let i = 0; i < tasks.length; i++) {
+	if (tasks[i] === 'Задача 2') {
+		break;
+	}
+	console.log(tasks[i]);
 }
