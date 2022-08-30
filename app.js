6c3000a5f0216c4234d0b49d880b7140040dee83
Alaricode <antonlarichev@gmail.com>
2022-08-30 16:05:36 +0300

    Циклы for of и for in

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-// for (let i = 1; i < 5; i++) {
-// 	console.log(`Вывод - ${i}`);
-// }
-
-// let i = 1;
-// while (i < 5) {
-// 	console.log(`Вывод - ${i}`);
-// 	i++;
-// }
-
-const arr = [1, 4, 8, 7];
+const arr = [1, 4, 8, 7, 'ddfg'];
 
 for (let i = 0; i < arr.length; i++) {
-	if (arr[i] > 5) {
-		break;
-	}
 	console.log(arr[i]);
 }
 
-let i = 0;
-while (arr[i] <= 5 && i < arr.length) {
-	console.log(arr[i]);
-	i++;
+for (let element of arr) {
+	console.log(element);
 }
 
-let j = 0;
-do {
-	console.log(j);
-	j++;
-} while(j < 0)
+for (let index in arr) {
+	console.log(arr[index]);
+}
