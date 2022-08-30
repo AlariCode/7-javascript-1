7f465505817cc368216922ce347514e481fc32ca
Alaricode <antonlarichev@gmail.com>
2022-08-30 15:56:15 +0300

    Цикл while

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 // for (let i = 1; i < 5; i++) {
-// 	console.log(`Цикл 1 - ${i}`);
-// 	for(let j = 1; j < 5; j++) {
-// 		console.log(`Цикл 2 - ${j}`);
-// 	}
+// 	console.log(`Вывод - ${i}`);
 // }
 
-const tasks = [[1, 'Задача 1'], [2, 'Задача 2']];
+// let i = 1;
+// while (i < 5) {
+// 	console.log(`Вывод - ${i}`);
+// 	i++;
+// }
 
-for (let i = 0; i < tasks.length; i++) {
-	for(let j = 0; j < tasks[i].length; j++) {
-		console.log(tasks[i][j]);
+const arr = [1, 4, 8, 7];
+
+for (let i = 0; i < arr.length; i++) {
+	if (arr[i] > 5) {
+		break;
 	}
+	console.log(arr[i]);
 }
+
+let i = 0;
+while (arr[i] <= 5 && i < arr.length) {
+	console.log(arr[i]);
+	i++;
+}
+
+let j = 0;
+do {
+	console.log(j);
+	j++;
+} while(j < 0)
