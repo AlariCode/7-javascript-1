154336d9e7946a1c62f9ea7601f69f1086b2513b
Alaricode <antonlarichev@gmail.com>
2022-10-11 20:20:11 +0300

    map

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const score = [5, 10, 0, 15];
+const transactionInUSD = [10, -7, 50, -10, 100];
 
-// for (const [i, el] of score.entries()) {
-// 	console.log(`Раунд ${i + 1}: ${el}`)
-// }
+const transactionInRUB = [];
+for (const transaction of transactionInUSD) {
+	transactionInRUB.push(transaction * 60);
+}
 
-score.forEach((el, i) => {
-	console.log(`Раунд ${i + 1}: ${el}`);
-});
-// (5, 0) => { ... }
-// (10, 1) => { ... }
+// console.log(transactionInUSD);
+// console.log(transactionInRUB);
+
+const transactionInRUB2 = transactionInUSD
+	.map((transaction, i) => {
+		console.log(i);
+		return transaction * 60
+	});
+
+console.log(transactionInUSD);
+console.log(transactionInRUB2);
