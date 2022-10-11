32906409726a88b343784e4d85ea5dd804334dc8
Alaricode <antonlarichev@gmail.com>
2022-10-11 17:33:16 +0300

    Возврат функции

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
-function add(a, b) {
-	return a + b;
+function power(pow) {
+	return function (num) {
+		return num**pow;
+	}
 }
 
-function subtract(a, b) {
-	return a - b;
-}
+const powerOfTwo = power(2);
+console.log(powerOfTwo(5));
+console.log(powerOfTwo(10));
 
-function power(a, b) {
-	return a**b;
-}
+const powerOfThree = power(3);
+console.log(powerOfThree(5));
 
-//Функция высшего порядка
-function calculate(a, b, fn) {
-	console.log(fn.name);
-	const res = fn(a, b);
-	return res;
-}
-
-let res = calculate(3, 5, add);
-console.log(res);
-res = calculate(3, 5, subtract);
-console.log(res);
-res = calculate(3, 5, power);
-console.log(res);
+console.log(power(5)(4));
