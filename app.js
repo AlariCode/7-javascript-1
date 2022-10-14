e8d7494b20f0eab1486f880b6a5646f45bd1770f
Alaricode <antonlarichev@gmail.com>
2022-10-14 17:48:17 +0300

    Строки и массивы

--- app.js
+++ app.js
@@ -1,46 +1,8 @@
-/* проверить является ли это номером телефона России */
-/* верные */
-const num1 = '89103235356';
-const num2 = '+79103235356';
-const num3 = '+7(910)3235356';
-const num4 = '+7(910) 323-53-56';
-const num5 = '  +7(910) 323-53-56  ';
-/* не верные */
-const num1Error = '89103235';
-const num2Error = '+7d910d323-53-56';
-const num3Error = '9+7103235356';
-const num4Error = '89103g35356';
-
-function isPhoneNumber(num) {
-	num = num.trim();
-	num = num.replace('+7', '8');
-	if (!num.startsWith('8')) {
-		return false;
-	}
-	num = num.replaceAll('(', '');
-	num = num.replaceAll(')', '');
-	num = num.replaceAll(' ', '');
-	num = num.replaceAll('-', '');
-	if (num.length != 11) {
-		return false;
-	}
-	let onlyNumber = true;
-	for (const char of num) {
-		if (isNaN(Number(char))) {
-			onlyNumber = false;
-			break;
-		}
-	}
-	return onlyNumber;
-}
-// true
-console.log(isPhoneNumber(num1));
-console.log(isPhoneNumber(num2));
-console.log(isPhoneNumber(num3));
-console.log(isPhoneNumber(num4));
-console.log(isPhoneNumber(num5));
-// false
-console.log(isPhoneNumber(num1Error));
-console.log(isPhoneNumber(num2Error));
-console.log(isPhoneNumber(num3Error));
-console.log(isPhoneNumber(num4Error));
+const userFullName = 'Вася Пупкин Васильевич';
+console.log(userFullName.split(' '));
+const [firstName, familyName, lastName] = userFullName.split(' ');
+console.log(firstName);
+console.log(familyName);
+console.log(lastName);
+const arr = ['Ты', 'знаешь', 'JS'];
+console.log(arr.join(' '))
