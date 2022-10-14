65d63dbe964769528832f8d8ec2369437a4e3e90
Alaricode <antonlarichev@gmail.com>
2022-10-14 20:11:15 +0300

    Упражнение - маскировка карты

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const film = 'Звёздные Воины';
-console.log(film.padStart(50, '*'))
-console.log(film.padEnd(50, '*'))
-console.log(film.repeat(10))
+/* Замаскировать всё, кроме последних 4х символов */
+const card = '2342834503458353';
+/* ************8353 */
+console.log(card.slice(-4).padStart(16, '*'));
