e784e4b3d9d4c028c7cb385a07002838f3dc221b
Alaricode <antonlarichev@gmail.com>
2022-10-17 18:12:47 +0300

    Упражнение - сортировка пользователей

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
-const user = {
-	name: 'Вася',
-	surname: 'Пупкин',
-	age: 24,
-	skills: [
-		'Программирование',
-		'Готовка'
-	],
-	eduBasic: 'Школа 10',
-	eduPro: 'МФТИ'
-};
-console.log(user.city);
-console.log(user.skills);
-console.log(user['skills']);
-const level = 'Pro'
-console.log(user['edu' + level]);
+/* отсортировать пользователей по возрасту */
 
-// const res = prompt('Введите свойство');
-user.city = 'Москва';
-user['city'] = 'Москва';
-console.log(user.city);
-
-user.age = 30;
-user['age'] = 30;
-console.log(user);
+const users = [
+	{ name: 'Вася', age: 30 },
+	{ name: 'Катя', age: 18 },
+	{ name: 'Аня', age: 40 },
+	{ name: 'Петя', age: 25 },
+];
+console.log(users.sort((a, b) => a.age - b.age));
