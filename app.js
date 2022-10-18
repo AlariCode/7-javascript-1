c6ca9f2bc96a7b90004b9cb0fc608ed01845faf1
Alaricode <antonlarichev@gmail.com>
2022-10-18 18:04:57 +0300

    Упражнение - склад

--- app.js
+++ app.js
@@ -1,17 +1,56 @@
-const cities = {
-	msk: {
-		temp: {
-			celcius: 25
-		}
+/*
+	Сделать объект склад с методами добавления на склад, поиска
+	по складку товара и расчёт веса
+*/
+const warehouse = {
+	goods: [],
+	findGoodById: function (id) {
+		return this.goods.find(g => g.id == id);
 	},
-	spb: {
-
+	addGood:  function (good) {
+		const existedGood = this.findGoodById(good.id)
+		if (existedGood) {
+			console.log('Этот товар уже есть на складе');
+			return;
+		}
+		this.goods.push(good);
+	},
+	getWeightKg:  function () {
+		return this.goods.reduce((acc, el) => 
+			acc += el.weight?.kg ? el.weight.kg : 0,
+			0
+		)
 	}
+};
+
+/* товары */
+const car = {
+	id: 1,
+	weight: {
+		kg: 1000
+	},
+	brand: 'Ford'
+};
+
+const chair = {
+	id: 2,
+	weight: {
+		kg: 2
+	},
 }
-const city = 'msk'
-if (cities[city] != undefined && cities[city].temp != undefined) {
-	console.log(cities[city].temp.celcius);
+const paper = {
+	id: 3,
+	color: 'red'
 }
 
-console.log(cities[city]?.temp?.celcius);
-
+warehouse.addGood(car);
+warehouse.addGood(car);
+warehouse.addGood(chair);
+warehouse.addGood(paper);
+console.log(warehouse.goods);
+let findedItem = warehouse.findGoodById(6);
+console.log(findedItem);
+findedItem = warehouse.findGoodById(1);
+console.log(findedItem);
+const w = warehouse.getWeightKg();
+console.log(w);
