769fecb25ae62d8802e4de0ba98c6a33a7f2ed68
Alaricode <antonlarichev@gmail.com>
2022-10-18 17:06:04 +0300

    Enhance object literals

--- app.js
+++ app.js
@@ -5,41 +5,9 @@
 	Так же реализовать метод вывода числа операций по кошельку
  */
 
+const initialBalance = 7;
+
 const wallet = {
-	balance: 0,
+	balance: initialBalance,
 	operations: [],
-	increase: function(sum, reason) {
-		this.balance += sum;
-		this.operations.push({
-			reason: reason,
-			sum: sum
-		})
-		return true;
-	},
-	decrease: function(sum, reason) {
-		if (this.balance < sum) {
-			console.log('Недостаточно баланса');
-			return false;
-		}
-		this.balance -= sum;
-		this.operations.push({
-			reason: reason,
-			sum: -sum
-		})
-		return true;
-	},
-	getOperationLenght: function () {
-		return this.operations.length;
-	}
 };
-
-console.log(wallet.increase(1000, 'Зарплата'));
-console.log(wallet.getOperationLenght());
-console.log(wallet.decrease(2000, 'Покупка ноутбука'));
-console.log(wallet.getOperationLenght());
-console.log(wallet.decrease(500, 'Покупка телефон'));
-console.log(wallet.getOperationLenght());
-console.log(wallet.balance);
-console.log(wallet.operations);
-
-
