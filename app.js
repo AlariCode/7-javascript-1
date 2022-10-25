6848e53a7923ed0e2a2be961c0965b98630b35aa
Alaricode <antonlarichev@gmail.com>
2022-10-25 19:47:09 +0300

    Call, apply

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 'use strict';
 
-const b = 1;
-
-const a = {
-	b,
-	getB: function () {
-		return this.b;
-	},
-	getBAlt() {
-		return this.b;
+const audi = {
+	make: 'Audi',
+	model: 'A3',
+	year: 2021,
+	damages: [],
+	addDamage(part, rate) {
+		console.log(
+			`У авто ${this.make} ${this.model} ${this.year} добавлено: повреждение ${part} со степенью ${rate}`
+			)
+		this.damages.push({
+			part,
+			rate
+		})
 	}
-}
+};
+// audi.addDamage('Капот', 1);
 
-console.log(a.getBAlt())
+const bmw = {
+	make: 'BMW',
+	model: 'X5',
+	year: 2022,
+	damages: [],
+};
+
+bmw.addDamage = audi.addDamage;
+// bmw.addDamage('Бампер', 2);
+
+const addDamageFunc = audi.addDamage;
+// addDamageFunc('Бампер', 2);
+addDamageFunc.call(bmw, ...['Бампер', 2]);
+addDamageFunc.call(audi, 'Бампер', 2);
+
+addDamageFunc.apply(bmw, ['Бампер', 2]);
+addDamageFunc.apply(audi, ['Бампер', 2]);
