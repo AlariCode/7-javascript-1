2130fe863f77450c573b1dd5177a3b0b5c9b2be5
Alaricode <antonlarichev@gmail.com>
2022-10-31 19:50:03 +0300

    Обработка нажатий

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 'use strict';
 
-const panelText = document.querySelector('.panel').innerText;
-console.log(panelText);
-document.querySelector('.panel').innerText = 'New text';
-document.querySelector('.input').value = 'Text';
+// document.querySelector('.button').addEventListener('click', function() {
+// 	const input = document.querySelector('.input').value;
+// 	if (!input) {
+// 		return;
+// 	}
+// 	document.querySelector('.panel').innerText = input;
+// 	document.querySelector('.input').value = '';
+// });
+
+function changeClick() {
+	const input = document.querySelector('.input').value;
+	if (!input) {
+		return;
+	}
+	document.querySelector('.panel').innerText = input;
+	document.querySelector('.input').value = '';
+}
