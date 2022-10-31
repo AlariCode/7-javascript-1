975ae77e8adf87ee9b4752f3befc0e11ac7f2f15
Alaricode <antonlarichev@gmail.com>
2022-10-31 17:15:09 +0300

    Упражнение - работа с замыканиями

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 'use strict';
 
-function changeBalance() {
-	let balance = 0;
-	let key = 'asd';
-	return function(sum) {
-		balance += sum;
-		key = 'adsf';
-		console.log(`Баланс: ${balance}`);
+/* 
+	Сделать функицю пользовтеля, которая берёт за основу
+	userInfo и за счёт замыкания создаёт новый объект, с
+	котором можно работать как user1().increse(100)
+*/
+
+const userInfo = {
+	balance: 0,
+	operations: 0,
+	increse(sum) {
+		this.balance +=sum;
+		this.operations++;
 	}
 }
 
-const change = changeBalance();
-change(100);
-change(-50);
-change(200);
-console.dir(change);
+function user() {
+	const userObj = {
+		balance: 0,
+		operations: 0,
+		increse(sum) {
+			this.balance +=sum;
+			this.operations++;
+		}
+	};
+	return function() {
+		return userObj;
+	}
+}
 
-const change2 = changeBalance();
-change2(100);
-console.dir(change2);
+const user1 = user();
+user1().increse(100);
+user1().increse(100);
+console.log(user1());
+
+const user2 = user();
+user2().increse(100);
+console.log(user2());
