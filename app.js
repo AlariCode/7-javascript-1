aed1501d8973f34e716de6cd26558e77e66d46be
Alaricode <antonlarichev@gmail.com>
2022-11-02 15:20:08 +0300

    Упражнение - получение DOM элементов

--- app.js
+++ app.js
@@ -7,13 +7,7 @@ function submitForm() {
 	}
 	document.querySelector('.panel').innerText = input;
 	document.querySelector('.input').value = '';
-	// document.querySelector('.notification').classList.add('notification_active');
-	// document.querySelector('.notification').classList.remove('notification_hidden');
-	console.log(document.querySelector('.notification').getAttribute('class'));
-	document.querySelector('.notification').setAttribute('class', 'notification')
-	document.querySelector('.notification').setAttribute('key', 1);
-	document.querySelector('.notification').setAttribute('user-id', 1);
-	console.log(Number(document.querySelector('.notification').getAttribute('user-id')));
+	document.querySelector('.notification').classList.remove('notification_hidden');
 }
 
 function inputChanged(e) {
@@ -21,3 +15,13 @@ function inputChanged(e) {
 		submitForm()
 	}
 }
+
+// console.log(document.querySelector('.one').innerText);
+// console.log(document.querySelector('.one ~ div').innerText);
+console.log(document.querySelectorAll('.one')[0].innerText);
+console.log(document.querySelectorAll('.one')[1].innerText);
+
+// console.log(document.querySelector('#two').innerText);
+console.log(document.getElementById('two').innerText);
+console.log(document.getElementsByClassName('one'));
+console.log(document.querySelector('[user-id="4"]').innerText);
