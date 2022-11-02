eab61aeedfae469b7312afbebd208264078c4a13
Alaricode <antonlarichev@gmail.com>
2022-11-02 14:55:59 +0300

    Установка атрибутов

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ function submitForm() {
 	document.querySelector('.panel').innerText = input;
 	document.querySelector('.input').value = '';
 	// document.querySelector('.notification').classList.add('notification_active');
-	document.querySelector('.notification').classList.remove('notification_hidden');
+	// document.querySelector('.notification').classList.remove('notification_hidden');
+	console.log(document.querySelector('.notification').getAttribute('class'));
+	document.querySelector('.notification').setAttribute('class', 'notification')
+	document.querySelector('.notification').setAttribute('key', 1);
+	document.querySelector('.notification').setAttribute('user-id', 1);
+	console.log(Number(document.querySelector('.notification').getAttribute('user-id')));
 }
 
 function inputChanged(e) {
