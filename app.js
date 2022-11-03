ba5c2a0a4ebc09eb8656487978cec2b08f34cf94
Alaricode <antonlarichev@gmail.com>
2022-11-03 18:16:39 +0300

    JSON

--- app.js
+++ app.js
@@ -16,10 +16,7 @@ function inputChanged(e) {
 	}
 }
 
-localStorage.setItem('token', 'sfgsdkf');
-localStorage.setItem('token1', 1);
-localStorage.setItem('token1', true);
-const token1 = localStorage.getItem('token1');
-console.log(typeof token1);
-localStorage.removeItem('token1');
-localStorage.clear();
+const obj = JSON.parse('{ "a": 1 }');
+console.log(obj.a);
+const str = JSON.stringify(obj);
+console.log(str)
