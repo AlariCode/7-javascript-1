632c8d28b1f45fa1bc9cdf13e4622abcb86065d2
Alaricode <antonlarichev@gmail.com>
2022-11-08 11:33:53 +0300

    FormData API

--- scripts/app.js
+++ scripts/app.js
@@ -88,6 +88,13 @@ function rerender(activeHabbitId) {
 	rerenderContent(activeHabbit);
 }
 
+/* work with days */
+function addDays(event) {
+	event.preventDefault();
+	const data = new FormData(event.target);
+	console.log(data.get('comment'));
+}
+
 /* init */
 (() => {
 	loadData();
