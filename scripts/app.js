f9e0a9490ce3ea3c2fb56d40cbb0aae72452cdea
Alaricode <antonlarichev@gmail.com>
2022-11-08 10:54:50 +0300

    Упражнение - рендер дней

--- scripts/app.js
+++ scripts/app.js
@@ -10,6 +10,10 @@ const page = {
 		h1: document.querySelector('.h1'),
 		progressPercent: document.querySelector('.progress__percent'),
 		progressCoverBar: document.querySelector('.progress__cover-bar'),
+	},
+	content: {
+		daysContainer: document.getElementById('days'),
+		nextDay: document.querySelector('.habbit__day')
 	}
 }
 
@@ -28,9 +32,6 @@ function saveData() {
 
 /* render */
 function rerenderMenu(activeHabbit) {
-	if (!activeHabbit) {
-		return;
-	}
 	for (const habbit of habbits) {
 		const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
 		if (!existed) {
@@ -54,9 +55,6 @@ function rerenderMenu(activeHabbit) {
 }
 
 function rerenderHead(activeHabbit) {
-	if (!activeHabbit) {
-		return;
-	}
 	page.header.h1.innerText = activeHabbit.name;
 	const progress = activeHabbit.days.length / activeHabbit.target > 1
 		? 100
@@ -65,10 +63,29 @@ function rerenderHead(activeHabbit) {
 	page.header.progressCoverBar.setAttribute('style', `width: ${progress}%`);
 }
 
+function rerenderContent(activeHabbit) {
+	page.content.daysContainer.innerHTML = '';
+	for (const index in activeHabbit.days) {
+		const element = document.createElement('div');
+		element.classList.add('habbit');
+		element.innerHTML = `<div class="habbit__day">День ${Number(index) + 1}</div>
+              <div class="habbit__comment">${activeHabbit.days[index].comment}</div>
+              <button class="habbit__delete">
+                <img src="./images/delete.svg" alt="Удалить день ${index + 1}" />
+              </button>`;
+		page.content.daysContainer.appendChild(element);
+	}
+	page.content.nextDay.innerHTML = `День ${activeHabbit.days.length + 1}`;
+}
+
 function rerender(activeHabbitId) {
 	const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
+	if (!activeHabbit) {
+		return;
+	}
 	rerenderMenu(activeHabbit);
 	rerenderHead(activeHabbit);
+	rerenderContent(activeHabbit);
 }
 
 /* init */
