e4556e4e1a30f60c97ff3dfcad38e3806d424b43
Alaricode <antonlarichev@gmail.com>
2022-11-08 16:11:31 +0300

    Упражнение - форма добавления привычки

--- scripts/app.js
+++ scripts/app.js
@@ -43,6 +43,35 @@ function togglePopup() {
 	}
 }
 
+function resetForm(form, fields) {
+	for (const field of fields) {
+		form[field].value = '';
+	}
+}
+
+function validateAndGetFormData(form, fields) {
+	const formData = new FormData(form);
+	const res = {};
+	for (const field of fields) {
+		const fieldValue = formData.get(field);
+		form[field].classList.remove('error');
+		if (!fieldValue) {
+			form[field].classList.add('error');
+		}
+		res[field] = fieldValue;
+	}
+	let isValid = true;
+	for (const field of fields) {
+		if (!res[field]) {
+			isValid = false;
+		}
+	}
+	if (!isValid) {
+		return;
+	}
+	return res;
+}
+
 /* render */
 function rerenderMenu(activeHabbit) {
 	for (const habbit of habbits) {
@@ -104,24 +133,21 @@ function rerender(activeHabbitId) {
 
 /* work with days */
 function addDays(event) {
-	const form = event.target;
 	event.preventDefault();
-	const data = new FormData(form);
-	const comment = data.get('comment');
-	form['comment'].classList.remove('error');
-	if (!comment) {
-		form['comment'].classList.add('error');
+	const data = validateAndGetFormData(event.target, ['comment']);
+	if (!data) {
+		return;
 	}
 	habbits = habbits.map(habbit => {
 		if (habbit.id === globalActiveHabbitId) {
 			return {
 				...habbit,
-				days: habbit.days.concat([{ comment }])
+				days: habbit.days.concat([{ comment: data.comment }])
 			}
 		}
 		return habbit;
 	});
-	form['comment'].value = '';
+	resetForm(event.target, ['comment']);
 	rerender(globalActiveHabbitId);
 	saveData();
 }
@@ -147,6 +173,26 @@ function setIcon(context, icon) {
 	const activeIcon = document.querySelector('.icon.icon_active');
 	activeIcon.classList.remove('icon_active');
 	context.classList.add('icon_active');
+}
+
+function addHabbit(event) {
+	event.preventDefault();
+	const data = validateAndGetFormData(event.target, ['name', 'icon', 'target']);
+	if (!data) {
+		return;
+	}
+	const maxId = habbits.reduce((acc, habbit) => acc > habbit.id ? acc : habbit.id, 0);
+	habbits.push({
+		id: maxId + 1,
+		name: data.name,
+		target: data.target,
+		icon: data.icon,
+		days: []
+	});
+	resetForm(event.target, ['name', 'target']);
+	togglePopup();
+	saveData();
+	rerender(maxId + 1);
 }
 
 /* init */
