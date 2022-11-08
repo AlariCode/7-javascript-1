9bcfcde4642dd4d82332af19fe7bf6884193d318
Alaricode <antonlarichev@gmail.com>
2022-11-08 14:57:00 +0300

    Упражнение - удаление дня

--- scripts/app.js
+++ scripts/app.js
@@ -71,7 +71,7 @@ function rerenderContent(activeHabbit) {
 		element.classList.add('habbit');
 		element.innerHTML = `<div class="habbit__day">День ${Number(index) + 1}</div>
               <div class="habbit__comment">${activeHabbit.days[index].comment}</div>
-              <button class="habbit__delete">
+              <button class="habbit__delete" onclick="deleteDay(${index})">
                 <img src="./images/delete.svg" alt="Удалить день ${index + 1}" />
               </button>`;
 		page.content.daysContainer.appendChild(element);
@@ -110,6 +110,21 @@ function addDays(event) {
 		return habbit;
 	});
 	form['comment'].value = '';
+	rerender(globalActiveHabbitId);
+	saveData();
+}
+
+function deleteDay(index) {
+	habbits = habbits.map(habbit => {
+		if (habbit.id === globalActiveHabbitId) {
+			habbit.days.splice(index, 1);
+			return {
+				...habbit,
+				days: habbit.days
+			};
+		}
+		return habbit;
+	});
 	rerender(globalActiveHabbitId);
 	saveData();
 }
